c08772bb8b5155c86671e6d98baa3142e4671109
MishaStryzhka <misha.strizhcko@gmail.com>
2023-02-22 03:15:29 +0100

    Update task 2

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,24 @@
+const refs = {
+    start: document.querySelector('button[data-start]'),
+    stop: document.querySelector('button[data-stop]'),
+    body: document.body,
+}
+let timerId = null;
 
+refs.start.addEventListener('click', () => {
+    timerId = setInterval(() => {
+        refs.body.style.backgroundColor = getRandomHexColor();
+    }, 1000);
+    refs.start.setAttribute("disabled", "")
+    refs.stop.removeAttribute("disabled")
+})
+
+refs.stop.addEventListener('click', () => {
+    clearInterval(timerId);
+    refs.stop.setAttribute("disabled", "")
+    refs.start.removeAttribute("disabled")
+})
+
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
---
+++ src/js/02-timer.js
@@ -0,0 +1,9 @@
+
+const refs = {
+    inputText: document.getElementById("datetime-picker"),
+    btnStart: document.querySelector('button[data-start]'),
+
+    // dataDaus: document.getElementById
+}
+
+console.log(refs.inputText);
