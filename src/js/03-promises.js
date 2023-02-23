f7207b42994ebe85f7ac1f1756ebe97b8d59faa9
MishaStryzhka <misha.strizhcko@gmail.com>
2023-02-23 06:46:10 +0100

    Update task-03

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,42 @@
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+const refs = {
+  form: document.querySelector('.form')
+}
+
+refs.form.addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  const {
+    elements: { delay, step, amount }
+  } = e.currentTarget;
+  let position = 1;
+  let totalDelay = delay.value;
+
+  setInterval(() => {
+    if (amount.value >= position) {
+      createPromise(position, totalDelay)
+        .then(({ position, delay }) => {
+          Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+        })
+        .catch(({ position, delay }) => {
+          Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+        });
+      totalDelay = +delay.value + +step.value * (position);
+      position += 1;
+    };
+  }, 0);
+});
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay)
+  })
 }
